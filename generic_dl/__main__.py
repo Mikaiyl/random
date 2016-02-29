40a9cf6b5d9563f530d053b9c6f65af73ad28597
Mikaiyl <mikaiyldavismorris.projects7@gmail.com>
2016-02-29 14:27:38 -0500

    {generic-dl}[Development A01] playing with new parser.

--- generic_dl/__main__.py
+++ generic_dl/__main__.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python2.7
-import urllib as url
+import urllib2 as url
 import bs4 
 import argparse
+from lxml import etree
 
-def getPage(url):
+def getPage(arg):
     req = url.Request(arg)
     response = url.urlopen(req)
     page = response.read()
-    page_bs = bs4.BeautifulSoup(page)
-    return pag_bs
+    return page
 
-if __name__ == "__main__":
+def getResponse(arg):
+    req = url.Request(arg)
+    response = url.urlopen(req)
+    return response
 
+def beautify(page_bs):
+    page_bs = bs4.BeautifulSoup(page,"lxml")
+    return page_bs
+
+def parse():
     parser = argparse.ArgumentParser(description='Find files with listed extensions in specified webpage.')
     parser.add_argument('-s','--site',dest='uri',required=True)
     parser.add_argument('-f','--format',dest='search_list')
+    return parser
 
-    parser.parse_args()
-    getPage(parser.uri)
+
+if __name__ == "__main__":
+
+    args = parse().parse_args()
+    page = getPage(args.uri)
+    print(page)
+    
+    print('-----------------------------------------BREAK-------------------------------------------------------')
+    
+    htmlparser = etree.HTMLParser()
+    tree = etree.parse(getResponse(args.uri), htmlparser)
